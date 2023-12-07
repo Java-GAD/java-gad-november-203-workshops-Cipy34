a03a3e67f84334f89217406552da2d2e0f71107d
Cipy348 <cipriangabriel112@gmail.com>
2023-12-07 20:48:05 +0200

    TrianglePattern

--- workshop/src/Main.java
+++ workshop/src/Main.java
@@ -11,8 +11,18 @@ public class Main {
             System.out.print("\n");
         }
     }
+
+    public static void printTrianglePattern(int lin){
+        for(int i = 1; i <= lin; i++) {
+            for(int j = 1; j <= i; j++)
+                System.out.print("*");
+            System.out.print("\n");
+        }
+    }
+
     public static void main(String[] args) {
         printHallowPattern(5, 5);
+        printTrianglePattern(5);
 
     }
 }
