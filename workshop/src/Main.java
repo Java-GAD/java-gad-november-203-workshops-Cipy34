58b43a1daccca5177296fabd032f76a96a368588
Cipy348 <cipriangabriel112@gmail.com>
2023-12-07 20:12:06 +0200

    HallowPattern

--- workshop/src/Main.java
+++ workshop/src/Main.java
@@ -1,17 +1,17 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Main {
-    public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+    public static void printHallowPattern(int lin, int col){
+        for(var i = 1; i <= lin; i++){
+            for(var j = 1; j <= col; j++){
+                if(i == 1 || j == 1 || i == lin || j == col)
+                    System.out.printf("*");
+                else
+                    System.out.printf(" ");
+            }
+            System.out.printf("\n");
         }
+    }
+    public static void main(String[] args) {
+        printHallowPattern(5, 5);
+
     }
 }
